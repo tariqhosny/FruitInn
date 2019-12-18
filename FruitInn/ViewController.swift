abc687b86f8691d8cae320bfbc81b4e2ec813f01
tariqhosny <tariqhosny@gmail.com>
2019-12-18 10:23:39 +0200

    Initial Commit

---
+++ FruitInn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FruitInn
+//
+//  Created by Tariq on 12/18/19.
+//  Copyright © 2019 Tariq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
